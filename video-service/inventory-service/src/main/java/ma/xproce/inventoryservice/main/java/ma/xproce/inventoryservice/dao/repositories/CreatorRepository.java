3ff2eb154524a579ddb64745459ae74b09853592
Xproce <badr.hirchoua@gmail.com>
2023-12-04 09:27:46 +0100

    Delete video-service/inventory-service/src/main/java/ma/xproce/inventoryservice/main directory

--- video-service/inventory-service/src/main/java/ma/xproce/inventoryservice/main/java/ma/xproce/inventoryservice/dao/repositories/CreatorRepository.java
+++
@@ -1,7 +0,0 @@
-package ma.xproce.inventoryservice.dao.repositories;
-
-import ma.xproce.inventoryservice.dao.entities.Creator;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CreatorRepository extends JpaRepository<Creator,Long> {
-}
--- video-service/inventory-service/src/main/java/ma/xproce/inventoryservice/main/java/ma/xproce/inventoryservice/dao/repositories/VideoRepository.java
+++
@@ -1,8 +0,0 @@
-package ma.xproce.inventoryservice.dao.repositories;
-
-import ma.xproce.inventoryservice.dao.entities.Creator;
-import ma.xproce.inventoryservice.dao.entities.Video;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VideoRepository extends JpaRepository<Video,Long> {
-}
